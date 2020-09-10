40251184f31032ff5105de65b02c687a8dbdafc5
DengTiansheng <979231935@qq.com>
2020-09-10 17:43:23 +0800

    修改新增课程设计界面布局

--- src/views/manage-course-design/index.vue
+++ src/views/manage-course-design/index.vue
@@ -1,33 +1,35 @@
 <template>
-  <el-container>
-    <el-header>
-      <el-button type="success" icon="el-icon-plus" @click="dialogFormVisible=true">新增</el-button>
-      <el-dialog title="新建课程设计" :visible.sync="dialogFormVisible">
-        <el-form :model="tableData" />
-      </el-dialog>
-    </el-header>
-    <el-main>
-      <el-table ref="multipleTable" :data="tableData.slice((currentPage-1)*pageSize,currentPage*pageSize)" style="width: 100%" :header-cell-style="{background:'#DCDFE6',color:'#303133'}" border>
-        <el-table-column type="index" :index="indexMethod" width="50" align="center" />
-        <el-table-column label="日期" width="180" prop="date" align="center">
-          <template slot-scope="scope">
-            <i class="el-icon-time" />
-            <span style="margin-left: 10px">{{ scope.row.date }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column label="创建人" width="180" prop="name" align="center" />
-        <el-table-column label="操作" align="center">
-          <template slot-scope="scope">
-            <el-button type="primary" icon="el-icon-edit" @click="$">编辑</el-button>
-            <el-button type="danger" icon="el-icon-delete" @click.native.prevent="deleteRow(scope.$index,tableData)">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </el-main>
-    <el-footer>
-      <el-pagination background layout="prev, pager, next" :total="tableData.length" :current-page="currentPage" :page-size="pageSize" @size-change="handleSizeChange" @current-change="handleCurrentChange" />
-    </el-footer>
-  </el-container>
+  <div class="app-container">
+    <el-container>
+      <el-header>
+        <el-button type="success" icon="el-icon-plus" @click="dialogFormVisible=true">新增</el-button>
+        <el-dialog title="新建课程设计" :visible.sync="dialogFormVisible">
+          <el-form :model="tableData" />
+        </el-dialog>
+      </el-header>
+      <el-main>
+        <el-table ref="multipleTable" :data="tableData.slice((currentPage-1)*pageSize,currentPage*pageSize)" style="width: 100%" :header-cell-style="{background:'#DCDFE6',color:'#303133'}" border>
+          <el-table-column type="index" :index="indexMethod" width="50" align="center" />
+          <el-table-column label="日期" width="180" prop="date" align="center">
+            <template slot-scope="scope">
+              <i class="el-icon-time" />
+              <span style="margin-left: 10px">{{ scope.row.date }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="创建人" width="180" prop="name" align="center" />
+          <el-table-column label="操作" align="center">
+            <template slot-scope="scope">
+              <el-button type="primary" icon="el-icon-edit" @click="$">编辑</el-button>
+              <el-button type="danger" icon="el-icon-delete" @click.native.prevent="deleteRow(scope.$index,tableData)">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-main>
+      <el-footer>
+        <el-pagination layout="prev, pager, next" :total="tableData.length" :current-page="currentPage" :page-size="pageSize" @size-change="handleSizeChange" @current-change="handleCurrentChange" />
+      </el-footer>
+    </el-container>
+  </div>
 </template>
 
 <script>
@@ -62,13 +64,22 @@ export default {
 </script>
 
 <style scoped>
-.el-header {
+.app-container {
+  padding: 12px;
+}
+
+.app-container .el-container {
+  background-color: #FFFFFF;
+  padding: 20px 20px 10px 20px;
+}
+
+.app-container .el-container .el-header {
   display: flex;
-  justify-content: flex-end;
+  justify-content: flex-start;
   align-items: center;
 }
 
-.el-footer {
+.app-container .el-container .el-footer {
   display: flex;
   justify-content: center;
   align-items: center;
