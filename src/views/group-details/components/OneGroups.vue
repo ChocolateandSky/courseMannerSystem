fec1bb4cdc2ec625e7469c104cb7e9ab0f26499d
ChocolateandSky <1017192349@qq.com>
2020-09-10 17:18:29 +0800

    补充

---
+++ src/views/group-details/components/OneGroups.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-container">
+    <h1>个人小组</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/group-details/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="app-container">
+    <div
+      class="filter-container"
+      :style="{height:autoHeight}"
+    >
+      <one-groups />
+    </div>
+  </div>
+</template>
+
+<script>
+import OneGroups from './components/OneGroups'
+
+export default {
+  name: 'GroupDetails',
+  components: {
+    OneGroups
+  },
+  data() {
+    return {
+      autoHeight: '200px'
+    }
+  },
+  mounted() {
+    this.getAutoHeight()
+    window.onresize = () => {
+      this.getAutoHeight()
+    }
+  },
+  methods: {
+    getAutoHeight() {
+      this.$nextTick(() => {
+        this.autoHeight = window.innerHeight - (100) + 'px'
+      })
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+ .app-container{
+    height: 100%;
+    width: 100%;
+    padding: 12px;
+    .filter-container{
+      align-items: center;
+      background-color: #fff ;
+      padding: 20px 20px 10px 20px;
+      font-size: 14px;
+    }
+ }
+</style>
