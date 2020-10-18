b3312e5e7cf24d82e357f75ae2107be57ea50213
刘镇猛 <1017192349@qq.com>
2020-10-18 22:37:10 +0800

    修改

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="navbar">
     <hamburger id="hamburger-container" :is-active="sidebar.opened" class="hamburger-container" @toggleClick="toggleSideBar" />
 
-    <breadcrumb id="breadcrumb-container" class="breadcrumb-container" />
+    <!-- <breadcrumb id="breadcrumb-container" class="breadcrumb-container" /> -->
 
     <div class="right-menu">
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
@@ -55,6 +55,7 @@ import Hamburger from '@/components/Hamburger'
 
 export default {
   components: {
+    // eslint-disable-next-line vue/no-unused-components
     Breadcrumb,
     Hamburger,
     UpdatePassword
